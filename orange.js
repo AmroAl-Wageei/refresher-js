9e73bc7b2166073200130eb1a223ef6b20825b9a
amro alwageei  -gt <gt.amro.alwageei@Jt.JtGroup>
2023-09-19 11:43:23 +0300

    Followup

--- orange.js
+++ orange.js
@@ -1,7 +1,213 @@
-// console.log("Amro");
+
+// JS REFRESH  : Function , Array , Object , Ternary , Spreed , Destructing Array , Distructing Object , Map
+
+//  Handle Error , DOM
 
 
 // #########################################################
+
+// #1 
+// function Laith( x , y ) {
+//     return x + y;
+// }
+// console.log(Laith(10 , 2));
+
+// #2
+//  const Yazan = function ( a , b ) {
+//     return a * b 
+// }
+// console.log(Yazan(5 , 5));
+
+// #3 Arrow 
+// const Maryana = () => { return }
+
+// #########################################################
+
+// #1 
+// if ( x > 10 ) {
+    
+// } else {
+    
+// }
+
+
+// console.log('Hello World');
+
+// x = 20;
+// y = 10;
+
+// x > 15 ? x =  x * 2 : x = x + 2;  // Condition , True , False 
+// console.log(x);
+
+// #########################################################
+
+// Array 
+// let colors = [ 'Red' , 20 , 'Yellow' , 'White'];
+// console.log(colors[3]);
+
+
+// // // Distructing Array
+// const [ A  , B , C , D  ] = colors;
+// console.log(B);
+
+
+// #########################################################
+// Object
+
+
+// const person1 = {
+//     name : "Anas",
+//     age : 30,
+//     isHabied : false,
+//     isShiekh : true,
+// }
+// const person2 = {
+//     name : "Ahmad",
+//     age : 18,
+//     isHabied : true,
+//     isShiekh : false,
+// }
+
+// console.log(person1);
+
+// const person2 = {...person1 , name : "Hanan" , age : 6}
+// console.log(person2);
+
+
+
+
+
+
+
+
+
+// const person2 = {...person1 , name : "Idress"}
+// console.log(person2);
+
+
+// const person3 = {...person2 , name : "Abd"};
+// console.log(person3);
+
+// #########################################################
+// Map 
+
+
+
+
+let colors = [ 1 , 2 , 3 , 4];
+
+// #1 Array Name
+// #2 map() , .filter() , .reduce()
+// #3 . (e)  Array Element / Indes
+// #4 => Arrow Function 
+// #5 {} Return 
+
+colors.map( (item) => {
+    console.log(item * 5); 
+})
+
+
+// // #1
+// colors.map( (e) => {
+//     console.log(`Color  : ${e}`);
+// })
+
+// // #2 Return Values with MAP in Object
+// const Mahmoud = colors.map( (e) => {
+//     return `Name :${ e} `;
+// })
+// console.log(Mahmoud);
+
+
+
+// #########################################################
+//  Different Between JS and RaectJS in Style 
+
+// camelCase
+// marginleft --- CSS + JS
+// marginLeft --- CSS + React
+
+
+// const StyleHeader = {
+//     borderRaduis : "50%",
+//     paddingTop : "10px",
+//     marginRight : "20rem",
+// }
+
+
+// <button  style{StyleHeader }> </button>
+
+// #########################################################
+
+
+
+
+
+
+
+
+
+
+
+
+// const a = person.name;
+// console.log(a);
+
+// const b = person.age;
+// console.log(b);
+
+// const c = person.isShiekh;
+// console.log(c);
+
+// #1
+// const a = person["name"];
+// console.log(a);
+
+// #2
+// const a = person[name];   Undefind
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+// #########################################################
+
+
+// function test9(d , x ) {
+//     return d * x
+// }
+// console.log(test9(3,4));
+
+
+// const Amro = function (a , b ) {
+//     return a + b;
+// }
+// console.log(Amro(3,3));
+
+
+// const Abd = ( x, y ) => {
+//     return x / y  ;
+// }
+// console.log(Abd(10 , 2));
+
+
+
+// A = 10;
+// B = 8;
+// const Abd1 = ( x , y ) => { return A < B ? A = x + A : y + B  }
+// console.log(Abd1(10 , 10));
+
+
+
+
 
 // Functions
 // 1 - Regular Function
@@ -27,13 +233,6 @@
 
 
 // #########################################################
-// Elements Events
-
-// <button onClick={() => {console.log("Hello World");}} >
-
-// </button>
-
-// #########################################################
 
 // Ternary Operator  " IF "
 
@@ -44,6 +243,38 @@
 
 // }
 
+
+// let player = prompt('Enter Your Name : ');
+// let result = (player == "Amro") ? 'This is Admin' : 'This is User';
+// console.log(result);
+
+
+// let x = 22;
+// let y = "Amro";
+
+// if (x > 20 ) {
+//     y = "Admin"
+// } else {
+//     y = "User"
+// }
+// console.log(y);
+
+
+
+// const Amro = () => {
+//     return <h1>Hello</h1>;    
+// }
+
+
+// let a = 26;
+// let b = a > 20 ? "hhhhh" : "ggggggg";
+
+// const Components = () => {
+//     return a < 20 ? ` <div> hhhhh </div>` : ` <div> ggggggg </div>` ;
+// }
+// console.log(Components());
+// console.log(a);
+// console.log(b);
 // 2 - Ternary
 // #EX1
 // let player = prompt("Enter Your T-SHIRT Number : ");
@@ -75,11 +306,11 @@
 
 // OBJECT
 
-const person = {
-    name : "Amro",
-    age : 26,
-    isCoach : true,
-}
+// const person = {
+//     name : "Amro",
+//     age : 26,
+//     isCoach : true,
+// }
 
 // const name = person.name;
 // console.log(name);
@@ -109,11 +340,31 @@ const person = {
 // const Coaches =["Rahaf" , ...Player]
 // console.log(Coaches);
 
+// destructing Array 
+
+
+// Destructuring the array
+// const [one, two, three] = Player;
+
+
+// console.log(one); // Osama
+// console.log(two); // Batool
+// console.log(three); // Amjad
+
+
+
+// const Player2 = [Player[2] = "Amro" , ...Player];
+// console.log(Player2);
+
+// const Player3 = [Player[0] = "Amro" , ...Player.slice(1)]
+// console.log(Player3);
+
+
 // #########################################################
 
 // MAP , FILTER , REDUCE 
 
-let colors = [ 'Red' , 'Green' , 'Yellow' , 'White' , 'Black' , "Brown" ];
+// let colors = [ 'Red' , 'Green' , 'Yellow' , 'White' , 'Black' , "Brown" ];
 
 // console.log(colors);
 
@@ -121,20 +372,13 @@ let colors = [ 'Red' , 'Green' , 'Yellow' , 'White' , 'Black' , "Brown" ];
 //     console.log("++" + e + "++");
 // })
 
+
+
+
 // const coloredObjects = colors.map((e) => {
 //     return  {colors : e};
 // })
 // console.log(coloredObjects);
 
-
-let names = [ 'Ahmad' , 'Ghaida' , 'Mahmoud' , 'Rand' , 'Yazan' , "Thar'a" ];
-
-names.filter((name) => {
-    console.log(name);
-})
-const filteredNames = names.filter((name)=> {
-    return name !== "Mahmoud";
-})
-console.log(filteredNames);
 
 // #########################################################
